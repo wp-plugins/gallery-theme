90f0d1f4fce48da54cdf5e800d6ecf0beae4a62c
massoud-mx <massoud-mx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-06 13:14:39 +0000

    = Version 1.1 = * One fixed issue.
    git-svn-id: https://plugins.svn.wordpress.org/gallery-theme/trunk@723495 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ themes/gallery-theme-default.php
@@ -0,0 +1,28 @@
+<?php
+/*
+<?php echo $image['thumb']; ?> --- getting thumbnail url of the image
+<?php echo $image['medium']; ?> --- getting medium-size url of the image
+<?php echo $image['large']; ?> --- getting large-size url of the image
+<?php echo $image['full']; ?> --- getting full-size url of the image
+<?php echo $image['width']; ?> --- getting full-size image width
+<?php echo $image['height']; ?> --- getting full-size image height
+<?php echo $image['link']; ?> --- getting image page link
+<?php echo $image['title']; ?> --- getting image title
+<?php echo $image['caption']; ?> --- getting image caption
+<?php echo $image['alt']; ?> --- getting image alternative text
+<?php echo $image['description']; ?> --- getting image description
+
+<?php echo $settings['link']; ?> --- getting link value from gallery settings
+<?php echo $settings['columns']; ?> --- getting columns value from gallery settings
+<?php echo $settings['orderby']; ?> --- getting sorting type from gallery settings
+*/
+?>
+<ul class="gallery-theme">
+	<?php foreach($images as $id): $image = gt_image($id); ?>
+	<li>
+		<a href="<?php echo $image['full']; ?>">
+			<img src="<?php echo $image['thumb']; ?>" alt="<?php echo $image['alt']; ?>" />
+		</a>
+	</li>
+	<?php endforeach; ?>
+</ul>
